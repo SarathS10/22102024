0deccb2e9e2598cf7b4923aa74d4b714c6805c47
Chandrasekar <s.b.chandrasekar@accenture.com>
2024-10-24 19:42:13 +0530

    update the code from loc repo

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "image_id" {
+  type = string
+  default = "ami-06b21ccaeff8cd686"
+}
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+variable "server_count" {
+  type = number
+  default = 3
+}
