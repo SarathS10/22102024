5e7ea3113e2eb1f3fca2f7defa07b0a87a1e6095
SarathS10 <sarath181410@gmail.com>
2024-10-27 17:38:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_subnet" "public1" {
 }
 resource "aws_subnet" "public2" {
   vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1a"
   tags = {
     Name = "Public Subnet_2"
