7d2b2cda75f0df312f796046fcbe7dc42707591e
SarathS10 <sarath181410@gmail.com>
2024-10-28 20:17:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,40 +5,40 @@ resource "aws_vpc" "VPC" {
   }
 }
 resource "aws_internet_gateway" "gw" {
-  vpc_id = "var.vpc_IGW"
+  vpc_id = "var.vpc-main"
 
   tags = {
     Name = "main"
   }
 }
 resource "aws_route_table" "example" {
-    vpc_id = "var.vpc_IGW"
+    vpc_id = "var.vpc-main"
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = "var.vpc_RT"
+        gateway_id = "var.vpc-gateway"
     }
     tags = {
         Name = "Sarath_RT"
     }
 }
-resource "aws_subnet" "main" {
-  vpc_id     = "var.vpc_IGW"
+resource "aws_subnet" "big" {
+  vpc_id     = "var.vpc-main"
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
   tags = {
     Name = "Sarath_pubS_1"
   }
 }
-resource "aws_subnet" "smain" {
-  vpc_id     = "var.vpc_IGW"
+resource "aws_subnet" "small" {
+  vpc_id     = "var.vpc-main"
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1a"
   tags = {
     Name = "Sarath_pubS_2"
   }
 }
-resource "aws_subnet" "mains" {
-  vpc_id     = "var.vpc_IGW"
+resource "aws_subnet" "logic" {
+  vpc_id     = "var.vpc-main"
   cidr_block = "10.0.3.0/24"
   availability_zone = "us-east-1a"
   tags = {
