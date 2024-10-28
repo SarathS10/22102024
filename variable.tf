92147d0450f9f1f1f539cf2c6abc158ac5872ebc
SarathS10 <sarath181410@gmail.com>
2024-10-28 19:03:16 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,12 +1,4 @@
-variable "image_id" {
-  type = string
-  default = "ami-06b21ccaeff8cd686"
-}
-variable "instance_type" {
-  type = string
-  default = "t2.micro"
-}
-variable "server_count" {
+variable "Cidr no" {
   type = number
-  default = 3
+  default = "10.0.0.0/16"
 }
