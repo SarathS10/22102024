2e934cec127a738245d7bfce573fd7bd034fab82
SarathS10 <sarath181410@gmail.com>
2024-10-29 11:55:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,62 +5,62 @@ resource "aws_vpc" "VPC" {
   }
 }
 resource "aws_internet_gateway" "gw" {
-  vpc_id = "vpc-0cbbf95b2ff5f575a"
+  vpc_id = var.vpc_value
 
   tags = {
     Name = "main"
   }
 }
 resource "aws_route_table" "example" {
-    vpc_id = "vpc-0cbbf95b2ff5f575a"
+    vpc_id = var.vpc_value
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = "igw-06b2beccfca006d1e"
+        gateway_id = var.gate_value
     }
     tags = {
         Name = "Sarath_RT"
     }
 }
 resource "aws_subnet" "big" {
-  vpc_id     = "vpc-0cbbf95b2ff5f575a"
+  vpc_id     = var.vpc_value
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = var.find_loc
   tags = {
     Name = "Sarath_pubS_1"
   }
 }
 resource "aws_subnet" "small" {
-  vpc_id     = "vpc-0cbbf95b2ff5f575a"
+  vpc_id     = var.vpc_value
   cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = var.find_loc
   tags = {
     Name = "Sarath_pubS_2"
   }
 }
 resource "aws_subnet" "logic" {
-  vpc_id     = "vpc-0cbbf95b2ff5f575a"
+  vpc_id     = var.vpc_value
   cidr_block = "10.0.3.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = var.find_loc
   tags = {
     Name = "Sarath_Private"
   }
 }
 resource "aws_route_table_association" "a" {
-  subnet_id      = "subnet-0729db33d757804bf"
-  route_table_id = "rtb-07a1a9aff726d91af"
+  subnet_id      = var.sub_value
+  route_table_id = var.RT_value
 }
 resource "aws_route_table_association" "c" {
-  subnet_id      = "subnet-07283665af9088b73"
-  route_table_id = "rtb-07a1a9aff726d91af"
+  subnet_id      = var.sub2_value
+  route_table_id = var.RT_value
 }
 resource "aws_route_table_association" "b" {
-  subnet_id      = "subnet-07bb4d45b9d15a8ff"
-  route_table_id = "rtb-07a1a9aff726d91af"
+  subnet_id      = var.sub3_value
+  route_table_id = var.RT_value
 }
 resource "aws_instance" "my_instance3" {
   ami           = var.image_id
   instance_type = var.instance_type
-  subnet_id = "subnet-07bb4d45b9d15a8ff"
+  subnet_id = var.sub3_value
   tags = {
     Name = "My Instance_private"
   }
@@ -68,7 +68,7 @@ resource "aws_instance" "my_instance3" {
 resource "aws_instance" "my_instance1" {
   ami           = var.image_id
   instance_type = var.instance_type
-  subnet_id = "subnet-07283665af9088b73"
+  subnet_id = var.sub2_value
   tags = {
     Name = "My Instance_1Pub"
   }
@@ -76,7 +76,7 @@ resource "aws_instance" "my_instance1" {
 resource "aws_instance" "my_instance2" {
   ami           = var.image_id
   instance_type = var.instance_type
-  subnet_id = "subnet-0729db33d757804bf"
+  subnet_id = var.sub_value
   tags = {
     Name = "My Instance_2Pub"
   }
