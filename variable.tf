bf37218517c3202e37db5ed78196050779ef308b
SarathS10 <sarath181410@gmail.com>
2024-10-29 11:54:39 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -6,3 +6,31 @@ variable "instance_type" {
   type = string
   default = "t2.micro"
 }
+variable "vpc_value" {
+  type = string
+  default = "vpc-0cbbf95b2ff5f575a"
+}
+variable "gate_value" {
+  type = string
+  default = "igw-06b2beccfca006d1e"
+}
+variable "find_loc" {
+  type = string
+  default = "us-east-1a"
+}
+variable "sub_value" {
+  type = string
+  default = "subnet-0729db33d757804bf"
+}
+variable "sub2_value" {
+  type = string
+  default = "subnet-07283665af9088b73"
+}
+variable "sub3_value" {
+  type = string
+  default = "subnet-07bb4d45b9d15a8ff"
+}
+variable "RT_value" {
+  type = string
+  default = "rtb-07a1a9aff726d91af"
+}
