5d2f0b3d86c716cd219c5113a3ad37d0556b08db
Chandrasekar <s.b.chandrasekar@accenture.com>
2024-11-05 22:14:05 +0530

    igw

--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output "vpc_id" {
   description = "VPC of ID"
   value       = resource.aws_vpc.VPC.id
 }
+output "vpc_id" {
+  description = "IGW of ID"
+  value       = resource.aws_internet_gateway.gw.id
+}
