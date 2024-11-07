7c0b0beaa3477cde0ccc61a98025ece09fb997b8
Chandrasekar <s.b.chandrasekar@accenture.com>
2024-11-07 19:11:25 +0530

    hello

--- variable.tf
+++ variable.tf
@@ -6,32 +6,3 @@ variable "instance_type" {
   type = string
   default = "t2.micro"
 }
-variable "vpc_value" {
-  type = string
-  default = "vpc-09dc513453d696629"
-}
-variable "gate_value" {
-  type = string
-  default = "igw-00a16c76d3fe3c570"
-}
-variable "find_loc" {
-  type = string
-  default = "us-east-1a"
-}
-variable "sub_value" {
-  type = string
-  default = "subnet-0e25a5575f46d6c0d"
-}
-variable "sub2_value" {
-  type = string
-  default = "subnet-0866f1032b4601136"
-}
-variable "sub3_value" {
-  type = string
-  default = "subnet-0862013fba6798e98"
-}
-variable "RT_value" {
-  type = string
-  default = "rtb-0c29041b2404ccb85"
-}
-
