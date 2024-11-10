1d7fdd2ba585680eec0cf9a8e2f7b8084a947424
Chandrasekar <s.b.chandrasekar@accenture.com>
2024-11-10 22:15:25 +0530

    sg

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "app_server" {
+resource "aws_instance" "hello" {
   ami           = var.image_id
   instance_type = var.instance_type
-  security_group_id = "sg-0aa5e1dadf8190df3"
+  security_groups = "aws_security_group.sg-0aa5e1dadf8190df3.id"
   tags = {
     Name = "HelloEC2"
   }
