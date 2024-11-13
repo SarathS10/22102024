655c32cee41e17cd97d63cddeb3bbd42f845eae2
Chandrasekar <s.b.chandrasekar@accenture.com>
2024-11-13 19:38:17 +0530

    Modules9

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ module "ec2-instance" {
   ami = data.aws_ami.ami19.id
   availability_zone = var.location
   instance_type = var.instance_type
+  instance_tags = ["poc"]
   vpc_security_group_ids =[module.security-group.security_group_id]
   tags ={
     Name = "sarath"
@@ -22,13 +23,18 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.15.0"
   cidr = "10.0.0.0/16"
-  private_subnets = ["private"]
+  private_subnets = ["10.0.1.0/24"]
+  availability_zone = var.location
+
+  tags={
+    Name = "sub"
+  }
   }
 
 module "security-group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
   description = "management"
-  vpc_id = [module.vpc.vpc_id]
+  vpc_id = module.vpc.vpc_id
   name = "Nan"
 }
