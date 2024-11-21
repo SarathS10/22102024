83063850926a4617e27d72b3b7a6f9312a4ec0e8
Chandrasekar <s.b.chandrasekar@accenture.com>
2024-11-21 20:11:58 +0530

    fbsa

--- main.tf
+++ main.tf
@@ -37,9 +37,3 @@ module "security-group" {
   vpc_id = module.vpc.vpc_id
   name = "Nan"
 }
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.29.0"
-  cluster_name = "Sarath"
-
-}
